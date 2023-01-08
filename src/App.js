37bee0135cf8315782938fbf67ff8200b0bac9f8
Godwin Idemudia <godwinide@gmail.com>
2023-01-08 06:32:57 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { Pannellum } from "pannellum-react";
+import img from './img/img.png'
 
 function App() {
+
+  const handleTwwet = color => {
+    const blueTweet = "I have chosen the blue pill, i will remain in the matrix, I'm an NPC after all."
+    const redTweet = "I have chosen the red pill, I am ready to break free from the matrix and enter the real world.";
+    window.open(`https://twitter.com/intent/tweet?text=${color === "blue" ? blueTweet : redTweet}`);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Pannellum
+      width="100vw"
+      height="100vh"
+      image={img}
+      pitch={0}
+      yaw={360}
+      hfov={170}
+      showZoomCtrl={false}
+      showFullscreenCtrl={false}
+      autoLoad
+      onLoad={() => {
+        console.log("panorama loaded");
+      }}
+    >
+      <Pannellum.Hotspot
+        type="custom"
+        pitch={-19.3}
+        yaw={362.7}
+        hfov={120}
+        text={"Blue Pill"}
+        handleClick={() => handleTwwet("blue")}
+        cssClass={"pill"}
+        name="blue"
+      />
+      <Pannellum.Hotspot
+        type="custom"
+        pitch={-19.3}
+        yaw={365.4}
+        hfov={120}
+        handleClick={() => handleTwwet("red")}
+        text={"Red Pill"}
+        cssClass={"pill"}
+        name="red"
+      />
+    </Pannellum>
   );
 }
 
