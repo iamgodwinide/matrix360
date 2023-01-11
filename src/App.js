64ee777e7029a31f556d69db9c5f9d53c5f254b6
Godwin Idemudia <godwinide@gmail.com>
2023-01-11 19:50:43 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Pannellum } from "pannellum-react";
 import img from './img/img.png'
 import audio from './audio/matrix.mp3'
+import redPill from './videos/red-pill-vista.mp4'
+import bluePill from './videos/matrix-agent-smith.mp4'
+import wearShade from './videos/the-matrix-reloaded-matrix.mp4'
+import matrixRain from './videos/code-purpose-of-life.mp4'
 import { useEffect, useRef, useState } from 'react';
+import { Progress } from 'reactstrap';
+
 
 function App() {
 
-  const [step, setStep] = useState(0);
+  const [step, setStep] = useState(-1);
   const [windowClassNames, setWindowClassNames] = useState(["", ""]);
   const [windowClassName, setWindowClassName] = useState("");
   const audioPlayer = useRef();
   const [answer, setAnswer] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  const mRain = useRef();
+  const rPill = useRef();
+  const bPill = useRef();
+  const wShade = useRef();
 
 
   const handleTwwet = color => {
@@ -31,6 +44,44 @@ function App() {
     }
   }
 
+  const handleRedEnded = () => {
+    rPill.current.style.zIndex = -10;
+    wShade.current.style.zIndex = 10;
+    setTimeout(() => {
+      wShade.current.play();
+    }, 200)
+  }
+
+  const handleBlueEnded = () => {
+    setTimeout(() => {
+      handleTwwet("blue");
+      bPill.current.style.zIndex = -10;
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000)
+    }, 500)
+  }
+
+  const handleShadeEnded = () => {
+    wShade.current.style.zIndex = -10;
+    mRain.current.style.zIndex = 10;
+    setTimeout(() => {
+      mRain.current.play();
+      setTimeout(() => {
+        handleTwwet("red");
+      }, 1000)
+    }, 200)
+  }
+
+  const chooseBlue = () => {
+    bPill.current.style.zIndex = 10;
+    bPill.current.play();
+  }
+
+  const chooseRed = () => {
+    rPill.current.style.zIndex = 10;
+    rPill.current.play();
+  }
 
   useEffect(() => {
     console.log(answer);
@@ -42,6 +93,24 @@ function App() {
       {step < 3
         &&
         <div className="q-wrap">
+          {
+            loading
+            &&
+            <div className='loading-wrap'>
+              <h1>The Real World</h1>
+              <Progress
+                animated
+                className="my-3"
+                value={100}
+                color="success"
+                style={{
+                  width: "20%",
+                  height: "2.3em",
+                  margin: "0 auto"
+                }}
+              />
+            </div>
+          }
           {
             step === 0
             &&
@@ -84,27 +153,78 @@ function App() {
         <div className={windowClassNames[0]}></div>
         <div className={windowClassNames[1]}></div>
       </div>
+      <div className='videos-wrap'>
+        <video className='v1'
+          src={bluePill}
+          preload={true}
+          controls={false}
+          ref={bPill}
+          onEnded={handleBlueEnded}
+        />
+        <video className='v2'
+          src={redPill}
+          preload={true}
+          controls={false}
+          ref={rPill}
+          onEnded={handleRedEnded}
+        />
+        <video className='v3'
+          src={wearShade}
+          preload={true}
+          controls={false}
+          ref={wShade}
+          onEnded={handleShadeEnded}
+        />
+        <video className='v4'
+          src={matrixRain}
+          preload={true}
+          controls={false}
+          ref={mRain}
+          loop={true}
+        />
+      </div>
       <Pannellum
-        width="100vw"
-        height="100vh"
+        width={window.innerWidth + "px"}
+        height={window.innerHeight + "px"}
         image={img}
-        pitch={-12}
+        pitch={-15}
         yaw={360}
-        hfov={100}
-        showZoomCtrl={false}
-        showFullscreenCtrl={false}
+        hfov={window.innerWidth < 1024 ? 10 : 90}
         autoLoad
+        author=""
+        title=""
+        orientationOnByDefault={false}
+        draggable
+        keyboardZoom
+        mouseZoom
+        preview=""
+        previewAuthor=""
+        previewTitle=""
+        showControls
+        showFullscreenCtrl={false}
+        showZoomCtrl={false}
         onLoad={() => {
-          console.log("panorama loaded");
+          setStep(0);
+          setLoading(false);
         }}
+        onScenechange={(id) => { console.log("Scene has change on " + id); }}
+        onScenechangefadedone={() => { console.log("panorama loaded"); }}
+        onError={(err) => { console.log("Error", err); }}
+        onErrorcleared={() => { console.log("Error Cleared"); }}
+        onMousedown={(evt) => { console.log("Mouse Down", evt); }}
+        onMouseup={(evt) => { console.log("Mouse Up", evt); }}
+        onTouchstart={(evt) => { console.log("Touch Start", evt); }}
+        onTouchend={(evt) => { console.log("Touch End", evt); }}
+        hotspotDebug={false}
       >
+
         <Pannellum.Hotspot
           type="custom"
           pitch={-19.3}
           yaw={355}
           hfov={120}
           text={"Blue Pill"}
-          handleClick={() => handleTwwet("blue")}
+          handleClick={chooseBlue}
           cssClass={"pill"}
           name="blue"
         />
@@ -113,11 +233,12 @@ function App() {
           pitch={-19.3}
           yaw={372}
           hfov={120}
-          handleClick={() => handleTwwet("red")}
+          handleClick={chooseRed}
           text={"Red Pill"}
           cssClass={"pill"}
           name="red"
         />
+
       </Pannellum>
     </div>
   );
